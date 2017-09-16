aca510f3cee7c7c6ad3f265ab4a8da7146350bfe
xiongzhengshen <31660517+xiongzhengshen@users.noreply.github.com>
2017-09-16 13:05:47 +0800

    Add files via upload

---
+++ 身份证判断性别.java
@@ -0,0 +1,41 @@
+package xiongzheng;
+
+import java.util.Scanner;
+
+public class JudgeSex.java {
+	public static void main(String[] args){
+		System.out.println("请输入身份证号码：");
+		Scanner in = new Scanner(System.in);
+		String a ;
+		char[] b ;
+		while(true){
+			a = in.nextLine();
+			b = a.toCharArray(); 
+			if(b.length!=18){
+				System.out.println("请输入18位身份证号:");
+				continue;
+			}
+			for(int i=0;i<18;i++){
+				if(!Character.isDigit(b[i])){
+					System.out.println("请输入正确的身份证号:");
+					continue;
+				}
+			}
+			if(!Character.isDigit(b[18])){
+				if(b[18]!='x'){
+					System.out.println("请输入正确的身份证号:");
+					continue;
+				}
+			}
+			if(b[17]%2==0){
+				System.out.println("性别：女");
+				break;
+			}
+			else{
+				System.out.println("性别：男");
+				break;
+			}
+		}
+		in.close();
+	}
+}
